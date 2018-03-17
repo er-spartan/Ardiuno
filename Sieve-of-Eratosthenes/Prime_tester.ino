2505d1a42c881e9d0cfc21097a3baa8e99102e49
er-spartan <37025835+er-spartan@users.noreply.github.com>
2018-03-17 12:56:41 -0700

    Add files via upload
    Implemented the sieve function

---
+++ Sieve-of-Eratosthenes/Prime_tester.ino
@@ -0,0 +1,31 @@
+void setup() {
+  // put your setup code here, to run once:
+  int Sprime = 2;
+  int Gnumber = 120;
+  bool Parray [Gnumber]; 
+  int multi = 2;
+}
+
+  int Sprime = 2;
+  int Gnumber = 120;
+  bool Parray [120]; 
+  int multi = 2;
+
+void sieve ()
+{
+  while (Sprime < Gnumber)
+    while (multi*Sprime <= Gnumber){
+    Parray[Sprime*multi] = false;
+    multi++;  
+  }
+ Sprime++;
+  for (; Parray[Sprime] = true;Sprime++){
+  }
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  sieve()
+}
+
+
